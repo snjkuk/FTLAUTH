7f40a9c42ca0d24a6b9d1648f0a0d7f3a69a92f4
Ravikant Rajak <er.ravi.rajak2790@gmail.com>
2017-02-28 11:52:08 +0530

    aaauth

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # get 'welcome/index'
+  root 'welcome#index'
+#   get 'auth/:provider/callback', to: 'sessions#create'
+# 	# get 'auth/:provider/callback', to: 'sessions#create'
+# 	# get 'auth/facbook/callback', to: 'sessions#facbook_signin'
+# get 'auth/failure', to: redirect('/')
+# get 'signout', to: 'sessions#destroy', as: 'signout'
+  # devise_for :users
+  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+    match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
